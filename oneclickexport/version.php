5c199fb417710bf25d6566389518c35e1776a031
Theemiss <midinfotn401@gmail.com>
2025-07-21 19:09:57 +0100

    once click backup

--- oneclickexport/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version   = 2023072200;
-$plugin->requires  = 2020061500; // Moodle 3.9+
-$plugin->component = 'local_oneclickexport';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0';
